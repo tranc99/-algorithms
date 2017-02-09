7b78087f276c85971812483ff3ce5a074bfce5cf
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-09 17:36:00 +0200

    add tests for the search function

--- linear_search.js
+++ linear_search.js
@@ -1,6 +1,6 @@
 var linearSearch = function(list, target) {
   var guess = 0;
-  for (var guess = 0; guess < list.length; guest++) {
+  for (var guess = 0; guess < list.length; guess++) {
     if (list[guess] === target) {
       console.log("found! at index ", guess);
       return guess;
--- test.js
+++ test.js
@@ -1,4 +1,14 @@
 var assert = require('assert');
 var linearSearch = require('./linear_search');
 
-describe()
+describe('Linear Search', function() {
+  describe('search', function() {
+    it('should return index of existing target', function() {
+      assert.equal(3, linearSearch.search([6, 8, 10, 12, 14, 16, 18], 12));
+    });
+
+    it('should return -1 when target is not in list', function() {
+      assert.equal(-1, linearSearch.search([2, 4, 6, 8], 5));
+    });
+  });
+});
