cacfa026102d47ec6ac6e4e333b6287bcbb208bd
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-09 17:20:09 +0200

    add the linear search algorithm

---
+++ linear_search.js
@@ -0,0 +1,15 @@
+var linearSearch = function(list, target) {
+  var guess = 0;
+  for (var guess = 0; guess < list.length; guest++) {
+    if (list[guess] === target) {
+      console.log("found! at index ", guess);
+      return guess;
+    }
+
+  }
+  return -1;
+}
+
+module.exports = {
+  linearSearch: linearSearch
+}
